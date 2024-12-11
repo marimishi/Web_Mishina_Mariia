006803ab2683a959567424d2c5f625bf1333b64b
marimishi <masimishi@gmail.com>
2024-12-11 09:24:06 +0500

    11

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,38 +1,124 @@
-import {TicTacToe} from "./components/TicTacToe.js";
+export const TicTacToe = {
+  el: null,
+  isGameEnd: false,
+  isXTurn: true,
+  matrix: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  wonCombinations: [
+    [[1, 1], [1, 2], [1, 3]],
+    [[2, 1], [2, 2], [2, 3]],
+    [[3, 1], [3, 2], [3, 3]],
+    [[1, 1], [2, 2], [3, 3]],
+    [[1, 3], [2, 2], [3, 1]],
+    [[1, 1], [2, 1], [3, 1]],
+    [[1, 2], [2, 2], [3, 2]],
+    [[1, 3], [2, 3], [3, 3]],
+  ],
 
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init)
-} else {
-    init()
-}
+  init({ el, onMove }) {
+    this.el = el;
+    this.onMove = onMove;
+    this.boxes = el.querySelectorAll('.tic-tac-toe__ceil');
+    return this;
+  },
 
-function init() {    
-    const moveEl = document.getElementById('move-value')
-    
-    const onMove = (isXTurn) => {
-        let currentMove
-        
-        if (isXTurn) {
-            currentMove = 'X'
-        } else {
-            currentMove = 'O'
+  initListeners() {
+    this.boxes.forEach(box => {
+      box.addEventListener('click', event => {
+        if (this.isGameEnd || !this.isBlockEmpty(event.target)) {
+          return;
         }
 
-        moveEl.innerText = currentMove
+        this.setBlockValue(event.target);
+        this.setBlockDom(event.target);
+
+        if (this.checkForWin()) {
+          this.setGameEndStatus();
+          setTimeout(() => alert('Победил ' + this.getCurrentTurnValue()));
+          return;
+        }
+
+        if (!this.checkHasEmptyBlocks()) {
+          this.setGameEndStatus();
+          setTimeout(() => alert('Конец игры')); 
+          return;
+        }
+
+        this.changeTurnValue();
+        if (this.onMove) {
+          this.onMove(this.isXTurn);
+        }
+      });
+    });
+  },
+
+  checkHasEmptyBlocks() {
+    return this.matrix.some(row => row.some(cell => cell === null));
+  },
+
+  startGame() {
+    this.initListeners();
+    this.onMove(this.isXTurn);
+  },
+
+  restartGame() {
+    this.matrix = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    this.isGameEnd = false;
+    this.isXTurn = true;
+    this.boxes.forEach(box => box.textContent = '');
+    if (this.onMove) this.onMove(this.isXTurn);
+  },
+
+  isBlockEmpty(target) {
+    const [row, col] = this.getBlockPosition(target);
+    return !this.matrix[row - 1][col - 1];
+  },
+
+  getBlockPosition(target) {
+    const { row, col } = target.dataset;
+    return [parseInt(row), parseInt(col)];
+  },
+
+  setBlockValue(target, clear = false) {
+    const [row, col] = this.getBlockPosition(target);
+    this.matrix[row - 1][col - 1] = clear ? null : this.getCurrentTurnValue();
+  },
+
+  setBlockDom(target, clear = false) {
+    target.textContent = clear ? '' : this.getCurrentTurnValue();
+  },
+
+  getCurrentTurnValue() {
+    return this.isXTurn ? 'X' : 'O';
+  },
+
+  changeTurnValue() {
+    this.isXTurn = !this.isXTurn;
+  },
+
+  checkForWin() {
+    for (let i = 0; i < this.wonCombinations.length; i++) {
+      const [first, second, third] = this.wonCombinations[i];
+
+      if (
+        this.matrix[first[0] - 1][first[1] - 1] &&
+        this.matrix[first[0] - 1][first[1] - 1] === this.matrix[second[0] - 1][second[1] - 1] &&
+        this.matrix[second[0] - 1][second[1] - 1] === this.matrix[third[0] - 1][third[1] - 1]
+      ) {
+        return true;
+      }
     }
+    return false;
+  },
 
-    const game = TicTacToe.init(
-        {
-            el: document.getElementById('tic-tac-toe'),
-            onMove,
-        }
-    )
-    
-    game.startGame()
-    
-    const restartBtn = document.getElementById('restart-btn')
-
-    restartBtn.addEventListener('click', () => {
-        game.restartGame()
-    })
-}
+  setGameEndStatus() {
+    this.isGameEnd = true;
+  },
+};
